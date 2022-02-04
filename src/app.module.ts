380b02f877c83c8b1255b0877629dbc89fa3b85f
maryadi <maryadi@kpiinstitute.com>
2022-02-04 09:56:24 +0700

    nest g module poipomodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PoipoController } from './poipo/poipo.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PoipomoduleModule } from './poipomodule/poipomodule.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-poipo')
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-poipo'),
+    PoipomoduleModule
   ],
   controllers: [AppController, PoipoController],
   providers: [AppService],
