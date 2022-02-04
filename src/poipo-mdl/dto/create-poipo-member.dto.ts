9424b04e575332f34d20bfde99b37bea84a4a0df
maryadi <maryadi@kpiinstitute.com>
2022-02-04 11:14:16 +0700

    add class validation and transformer

--- src/poipo-mdl/dto/create-poipo-member.dto.ts
+++ src/poipo-mdl/dto/create-poipo-member.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator'
+
 export class CreatePoipoMemberDto {
+    @IsString()
     readonly name: string
+
+    @IsString()
     readonly position: string
+
+    @IsString({ each: true })
     readonly skills: string[]
 }
